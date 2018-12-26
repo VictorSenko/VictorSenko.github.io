e0eb67239d3fc2aaf3781d47d3899abc00685bd8
Victor Senko <Victor_senko@mail.ru>
2018-12-27 01:18:48 +0300

    added styles for mobile

--- js/main.js
+++ js/main.js
@@ -1,21 +1,62 @@
 window.onload = function(){
-    let navBarIcon = document.getElementsByClassName('navbar__icon')[0];
-    
-    navBarIcon.addEventListener('click',()=>{
-        console.log(this);
-        let item = document.getElementsByClassName('navbar__links')[0];
-        if(item.style.display === 'none'){
-            item.style.display = 'flex';
+    function events(){
+        let navBarIcon = document.getElementsByClassName('navbar__icon')[0];
+        let sliderTabs = document.getElementsByClassName('main__features-tab');
+        let sliderContent = document.getElementsByClassName('main__features-transition-block')[0];
+        let resize = null;
+        
+        sliderTabs[0].style.backgroundColor = '#ff8b38';
+        
+        navBarIcon.addEventListener('click',()=>{
+            console.log(this);
+            let item = document.getElementsByClassName('navbar__links')[0];
+            if(item.style.display === 'none'){
+                item.style.display = 'flex';
+            }
+            else
+                item.style.display = 'none';
+
+        });
+        window.addEventListener('resize',()=>{
+             if(window.innerWidth > 765){
+                 document.getElementsByClassName('navbar__links')[0].style.display = "flex";
+             }else
+                 document.getElementsByClassName('navbar__links')[0].style.display = "none";
+             let width = document.getElementsByClassName('transition-block')[0].offsetWidth; 
+             resize(width);
+        });
+        
+        for(let i = 0; i < sliderTabs.length; i++){
+            sliderTabs[i].addEventListener('mouseenter',function(){
+                let width = document.getElementsByClassName('transition-block')[0].offsetWidth;
+                let siblings = this.parentNode.children;
+                let index = getElementIndex(this);
+                
+                function getElementIndex(node) {
+                    let index = 0;
+                    while (node = node.previousElementSibling) {
+                        index++;
+                    }
+                    return index;
+                };
+                
+                
+                resize = function resizeText (width){
+                    sliderContent.style.left = -width*index +'px';
+                };
+                
+                
+                
+                for(let i = 0; i < siblings.length; i++){
+                   siblings[i].style.backgroundColor = "#333333"; 
+                };
+                
+                sliderContent.style.left = -width*index +'px';
+                this.style.backgroundColor = '#ff8b38';
+            });
         }
-        else
-            item.style.display = 'none';
-       
-    });
-    window.addEventListener('resize',()=>{
-         if(window.innerWidth > 765){
-             document.getElementsByClassName('navbar__links')[0].style.display = "flex";
-         }else
-             document.getElementsByClassName('navbar__links')[0].style.display = "none";
-    })
+    };
+    events();
+    
     
 };
