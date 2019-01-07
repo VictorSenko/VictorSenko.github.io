c7cefbb2c5e63a431366d95a848f45dc83878251
Victor Senko <Victor_senko@mail.ru>
2019-01-07 21:30:30 +0300

    fixed onload

--- js/main.js
+++ js/main.js
@@ -1,53 +1,50 @@
-window.onload = function(){
-    function events(){
-        let navBarIcon = document.getElementsByClassName('navbar__icon')[0];
-        let sliderTabs = document.getElementsByClassName('main__features-tab');
-        let sliderContent = document.getElementsByClassName('main__features-transition-block')[0];
-        
-        sliderTabs[0].style.backgroundColor = '#ff8b38';
-        
-        navBarIcon.addEventListener('click',()=>{
-            let item = document.getElementsByClassName('navbar__links')[0];
-            if(item.style.display === 'none'){
-                item.style.display = 'flex';
-            }
-            else
-                item.style.display = 'none';
+function events(){
+    let navBarIcon = document.getElementsByClassName('navbar__icon')[0];
+    let sliderTabs = document.getElementsByClassName('main__features-tab');
+    let sliderContent = document.getElementsByClassName('main__features-transition-block')[0];
 
-        });
-        window.addEventListener('resize',()=>{
-             if(window.innerWidth > 765){
-                 document.getElementsByClassName('navbar__links')[0].style.display = "flex";
-             }else
-                 document.getElementsByClassName('navbar__links')[0].style.display = "none";
-             
-        });
-        
-        for(let i = 0; i < sliderTabs.length; i++){
-            sliderTabs[i].addEventListener('mouseenter',function(){
-                let width = document.getElementsByClassName('transition-block')[0].offsetWidth;
-                let siblings = this.parentNode.children;
-                let index = getElementIndex(this);
-                
-                function getElementIndex(node) {
-                    let index = 0;
-                    while (node = node.previousElementSibling) {
-                        index++;
-                    }
-                    return index;
-                };
-                
-               
-                for(let i = 0; i < siblings.length; i++){
-                   siblings[i].style.backgroundColor = "#333333"; 
-                };
-                
-                sliderContent.style.left = -width*index +'px';
-                this.style.backgroundColor = '#ff8b38';
-            });
+    sliderTabs[0].style.backgroundColor = '#ff8b38';
+
+    navBarIcon.addEventListener('click',()=>{
+        let item = document.getElementsByClassName('navbar__links')[0];
+        if(item.style.display === 'none'){
+            item.style.display = 'flex';
         }
-    };
-    events();
-    
-    
+        else
+            item.style.display = 'none';
+
+    });
+    window.addEventListener('resize',()=>{
+         if(window.innerWidth > 765){
+             document.getElementsByClassName('navbar__links')[0].style.display = "flex";
+         }else
+             document.getElementsByClassName('navbar__links')[0].style.display = "none";
+
+    });
+
+    for(let i = 0; i < sliderTabs.length; i++){
+        sliderTabs[i].addEventListener('mouseenter',function(){
+            let width = document.getElementsByClassName('transition-block')[0].offsetWidth;
+            let siblings = this.parentNode.children;
+            let index = getElementIndex(this);
+
+            function getElementIndex(node) {
+                let index = 0;
+                while (node = node.previousElementSibling) {
+                    index++;
+                }
+                return index;
+            };
+
+
+            for(let i = 0; i < siblings.length; i++){
+               siblings[i].style.backgroundColor = "#333333"; 
+            };
+
+            sliderContent.style.left = -width*index +'px';
+            this.style.backgroundColor = '#ff8b38';
+        });
+    }
 };
+events();
+  
